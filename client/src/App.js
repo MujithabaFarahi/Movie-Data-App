65cfa1bba66b7266c48cfc0859767dfe2150da8f
MujithabaFarahi <mujithaba2001@gmail.com>
2024-02-29 19:17:59 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">MoviesData</a>
+      </header>
+      <main>
+        List Movies
+      </main>
+    </div>
+  );
+}
+
+export default App;
